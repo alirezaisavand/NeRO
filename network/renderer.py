e0b61ce62ffbb198a816d92ffefde9d4df24c0dc
alirezaisavand <isavand.alireza@gmail.com>
2024-11-14 04:00:54 -0600

    add opt.act_super to default configs

--- network/renderer.py
+++ network/renderer.py
@@ -29,6 +29,7 @@ class ConfigWrapper:
         self.point_hyper_dim = 256
         self.view_ori = 0
         self.agg_weight_norm = 1
+        self.act_super = 1
         # Set each key-value pair in the dictionary as an attribute
         for key, value in config_dict.items():
             setattr(self, key, value)
