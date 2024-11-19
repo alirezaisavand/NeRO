8b1cb04eea2cae18f5d0c8de5f3a2fbc42f5c37b
alirezaisavand <isavand.alireza@gmail.com>
2024-11-19 04:35:07 -0600

    debugging

--- network/field.py
+++ network/field.py
@@ -972,7 +972,8 @@ class MCShadingNetwork(nn.Module):
             torch.Tensor: Tensor of shape (M, k, E) containing the embeddings of the
                           k-nearest neighbors for each point in pts.
         """
-        pts_np = pts.cpu().numpy()  # Convert query points to NumPy
+        pts_np = pts.cpu().numpy().astype(np.float32)  # Convert query points to NumPy
+        print(pts_np.dtype, pts_np.shape)
         distances, indices = kdtree.search(pts_np, k)  # Perform k-NN search
         indices_tensor = torch.tensor(indices, device=pts.device)  # Convert indices to tensor
         embeddings = neural_points.points_embeding[0][indices_tensor]
